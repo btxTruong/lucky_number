83cfef31c5935d19a15b26814f46e9c138ee05e0
Peter Pan <50477534+btxTruong@users.noreply.github.com>
2019-05-28 13:22:37 +0700

    update

--- ketqua.py
+++ ketqua.py
@@ -1,45 +1,36 @@
-import requests
-import bs4
-import log
 import argparse
+import bs4
+import requests
+import log
 
 from collections import defaultdict
 
 logger = log.get_logger(__name__)
 
 
-def respone_from_url(url, **kwargs):
+def crawl_prizes():
     """
-    Return response from url using requests module
-
-    :param url: (string) **(Required)** A valid url need to fetch data
-    :param redirect: (bool) (optional) Allow-redirect. Default is True
-    :param timeout: (int) (optional) Time to wait connection. Default is 5
-    :return: requests.models.Response
+    Using beautifulSoup to filter prizes from requests
+    :param response: (string) Respone from requests
+    :return: (list) Data after filter
     """
 
-    redirect = kwargs.get('redirect', True)
-    timeout = kwargs.get('timeout', 5)
+    base = 'http://ketqua.net'
 
-    if not url:
-        raise ValueError('No url provied')
+    response = requests.get(base)
 
-    if not url.startswith(('http://', 'https://')):
-        url = ''.join(('http://', url))
+    tree = bs4.BeautifulSoup(response.text, 'html.parser')
+    data = tree.select('td[id*=rs]')
 
-    try:
-        r = requests.get(url, allow_redirects=redirect, timeout=timeout)
-    except requests.exceptions.Timeout:
-        logger.error('Timeout')
-        raise
-    except requests.exceptions.InvalidURL:
-        logger.error('Invalid URL')
-        raise
+    if not data:
+        raise ValueError('No data found')
 
-    if r.status_code != 200:
-        return False
+    all_prizes = defaultdict(list)
+    for prize in data:
+        name_prize = prize.get('id')[3]
+        all_prizes[name_prize].append(prize.text)
 
-    return r
+    return all_prizes
 
 
 def check_lucky_money(nums):
@@ -51,22 +42,7 @@ def check_lucky_money(nums):
              (dict) If no number win prize
     """
 
-    if not nums:
-        raise ValueError('No numbers provided')
-
-    base_url = 'ketqua.net'
-    r = respone_from_url(base_url)
-
-    tree = bs4.BeautifulSoup(r.text, 'html.parser')
-    data = tree.select('td[id*=rs]')
-
-    if not data:
-        raise ValueError('No data found')
-
-    all_prizes = defaultdict(list)
-    for prize in data:
-        name_prize = prize.get('id')[3]
-        all_prizes[name_prize].append(prize.text)
+    all_prizes = crawl_prizes()
 
     # amount number digits need to check in prize
     match_number = {
@@ -88,8 +64,8 @@ def check_lucky_money(nums):
         for prize, number in all_prizes.items():
             if str(num)[-match_number[prize]:] in number:
                 winning_numbers.append(
-                    'Congratulations {} has won {}'
-                    .format(num, int(prize) + 1))
+                    'Congratulations {} has won {}'.format(num,
+                                                           int(prize) + 1))
                 break  # a number just win only one prize
 
     if winning_numbers:
